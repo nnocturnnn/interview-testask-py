b11b8ffc50d55990ea313f9e07fbbca0cd2ca400
nnocturnnn <vikchehovich@gmail.com>
2021-04-04 17:11:39 +0300

    wind

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 import sys 
 from PyQt5 import QtWidgets
-import ui.mainWindow 
-import ui.dataWindow
+import ui.mainWindow as mainWindow
 
-class MainWindow(QtWidgets.QMainWindow, ui.mainWindow.Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.searchButton.clicked.connect(self.gotoDataWindow)
-
-
-    def gotoDataWindow(self):
-        dataWindow = DataWindow()
-        widget.addWidget(dataWindow)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-        
-
-class DataWindow(QtWidgets.QMainWindow, ui.dataWindow.Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.gotoMainWindow)
-
-
-    def gotoMainWindow(self):
-        mainWindow = MainWindow()
-        widget.addWidget(mainWindow)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-
-
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = mainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    widget = QtWidgets.QStackedWidget()
-    mainWindow = MainWindow()
-    dataWindow = DataWindow()
-    widget.addWidget(mainWindow)
-    widget.addWidget(dataWindow)
-    widget.show()
-    app.exec_()
+    main() 
