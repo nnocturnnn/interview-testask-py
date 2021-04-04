ee6c1b35e7ffb4339e0d9c1c86680027859ed5d7
nnocturnnn <vikchehovich@gmail.com>
2021-04-04 17:47:00 +0300

    control window

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import sys 
 from PyQt5 import QtWidgets
 import ui.mainWindow as mainWindow
+import ui.dataWindow as dataWindow
+
+class WeatherMainWindow(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.searchButton.clicked.connect(self.gotoDataWindow)
+
+
+    def gotoDataWindow(self):
+        self.close()
+        city = self.seachBox.text()
+        self.twoWindow = WeatherDataWindow(city)
+        self.twoWindow.show()
+        
+
+class WeatherDataWindow(QtWidgets.QMainWindow, dataWindow.Ui_MainWindow):
+    data = ""
+    def __init__(self,data):
+        super().__init__()
+        self.setupUi(self)
+        self.data = data
+        self.pushButton.clicked.connect(self.gotoMainWindow)
+
+    def gotoMainWindow(self):
+        self.close()
+        self.twoWindow = WeatherMainWindow()
+        self.twoWindow.show()
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = mainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+    window = WeatherMainWindow()
+    window.show()
+    app.exec_()
 
 if __name__ == '__main__':
     main() 
