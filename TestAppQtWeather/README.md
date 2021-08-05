f27890623febb990a538bf8bb0caddb971465a6d
nnnoc <vikchehovich@gmail.com>
2021-08-05 18:50:47 +0300

    start comm

---
+++ TestAppQtWeather/README.md
@@ -0,0 +1,4 @@
+# TestPy
+## Simple Weather App
+## PyQt5
+![app](https://github.com/nnocturnnn/TestPy/blob/master/img/app.png)
---
+++ TestAppQtWeather/install.sh
@@ -0,0 +1,6 @@
+pip3 install virtualenv
+python3 -m venv env
+source env/bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+python3 main.py
